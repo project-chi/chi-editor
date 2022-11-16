6fce9084ac2809edd5fce33e15aa4214250916a3
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-11-16 12:55:36 +0700

    delete coordinates

--- chi_editor/chemistry/atom.py
+++ chi_editor/chemistry/atom.py
@@ -1,9 +1,5 @@
 class Atom:
     name: str
-    x: float
-    y: float
 
     def __init__(self, name: str, x: int, y: int) -> None:
         self.name = name
-        self.x = x
-        self.y = y
