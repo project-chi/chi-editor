b03ab1d33af44ab33321140b126078b02903da01
Dmitriy Vashurin <86285206+kapkekes@users.noreply.github.com>
2022-11-29 23:42:48 +0700

    Update canvas.py
    - Change __init__ methods
    - Rename attributes
    - Remove class attributes

--- chi_editor/canvas.py
+++ chi_editor/canvas.py
@@ -4,33 +4,34 @@ from PyQt6.QtWidgets import QLabel
 
 
 class Canvas(QLabel):
-    def __init__(self, parent=None):
-        super().__init__(parent)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.canvas = QtGui.QPixmap(400, 300)
         self.canvas.fill(QtGui.QColor("white"))
         self.setPixmap(self.canvas)
 
     def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
-        text_widget = DragLabel(self)
-        text_widget.setText("Some text")
+        text_widget = DragLabel(self, text='Some text')
         text_widget.move(ev.pos())
         text_widget.show()
 
 
 class DragLabel(QLabel):
-    mouse_move_start = False
-    delta = QPoint()
-
-    def __init__(self, parent):
-        super().__init__(parent)
+    is_moving: bool
+    delta: QPoint
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.is_moving = False
+        self.delta = QPoint()
 
     def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
-        self.mouse_move_start = True
+        self.is_moving = True
         self.delta = self.pos() - ev.scenePosition().toPoint()
 
     def mouseMoveEvent(self, ev: QtGui.QMouseEvent) -> None:
-        if self.mouse_move_start:
+        if self.is_moving:
             self.move(ev.scenePosition().toPoint() + self.delta)
 
     def mouseReleaseEvent(self, ev: QtGui.QMouseEvent) -> None:
-        self.mouse_move_start = False
+        self.is_moving = False
