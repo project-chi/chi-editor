4d5b1f196c971d37b066fd2345e9372aa278b939
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-11-29 23:19:23 +0700

    Create DragLabel which is movable widget

--- chi_editor/canvas.py
+++ chi_editor/canvas.py
@@ -1,15 +1,36 @@
-from PyQt6.QtGui import QColor, QPixmap, QMouseEvent, QPalette
-from PyQt6.QtWidgets import QLabel, QLineEdit
+from PyQt6 import QtGui
+from PyQt6.QtCore import QPoint
+from PyQt6.QtWidgets import QLabel
 
 
 class Canvas(QLabel):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.canvas = QPixmap(400, 300)
-        self.canvas.fill(QColor("white"))
+        self.canvas = QtGui.QPixmap(400, 300)
+        self.canvas.fill(QtGui.QColor("white"))
         self.setPixmap(self.canvas)
 
-    def mousePressEvent(self, ev: QMouseEvent) -> None:
-        text_widget = QLabel("Some text", self)
+    def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
+        text_widget = DragLabel(self)
+        text_widget.setText("Some text")
         text_widget.move(ev.pos())
         text_widget.show()
+
+
+class DragLabel(QLabel):
+    mouse_move_start = False
+    delta = QPoint()
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
+        self.mouse_move_start = True
+        self.delta = self.pos() - ev.scenePosition().toPoint()
+
+    def mouseMoveEvent(self, ev: QtGui.QMouseEvent) -> None:
+        if self.mouse_move_start:
+            self.move(ev.scenePosition().toPoint() + self.delta)
+
+    def mouseReleaseEvent(self, ev: QtGui.QMouseEvent) -> None:
+        self.mouse_move_start = False
