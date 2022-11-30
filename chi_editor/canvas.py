140aeef5a980a505c28dea0450648d4cfd704f3b
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-11-30 11:00:42 +0700

    Add custom text typing while text widget creation

--- chi_editor/canvas.py
+++ chi_editor/canvas.py
@@ -1,25 +1,12 @@
 from PyQt6 import QtGui
 from PyQt6.QtCore import QPoint
-from PyQt6.QtWidgets import QLabel
-
-
-class Canvas(QLabel):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.canvas = QtGui.QPixmap(400, 300)
-        self.canvas.fill(QtGui.QColor("white"))
-        self.setPixmap(self.canvas)
-
-    def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
-        text_widget = DragLabel(self, text='Some text')
-        text_widget.move(ev.pos())
-        text_widget.show()
+from PyQt6.QtWidgets import QLabel, QLineEdit
 
 
 class DragLabel(QLabel):
     is_moving: bool
     delta: QPoint
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.is_moving = False
@@ -35,3 +22,29 @@ class DragLabel(QLabel):
 
     def mouseReleaseEvent(self, ev: QtGui.QMouseEvent) -> None:
         self.is_moving = False
+
+
+class Canvas(QLabel):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.canvas = QtGui.QPixmap(400, 300)
+        self.canvas.fill(QtGui.QColor("white"))
+        self.setPixmap(self.canvas)
+
+    def mousePressEvent(self, ev: QtGui.QMouseEvent) -> None:
+        text_widget = DragLabel(self, text="Default text")
+        text_widget.move(ev.pos())
+        text_widget.show()
+
+        text_line = QLineEdit(self)
+        text_line.show()
+        text_line.setFocus()
+        text_line.move(ev.pos())
+        text_line.returnPressed.connect(lambda: self.read_line(text_line, text_widget))
+
+    def read_line(self, text_line: QLineEdit, text_widget: DragLabel) -> None:
+        text_widget.setText(text_line.text())
+        text_widget.adjustSize()
+
+        text_line.close()
