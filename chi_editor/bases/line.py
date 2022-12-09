5f5e13a02ae1374e4c77082fe56f708550f7a212
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-09 21:48:45 +0700

    Added texture field and virtual method fill_texture

--- chi_editor/bases/line.py
+++ chi_editor/bases/line.py
@@ -1,8 +1,16 @@
 from pathlib import Path
 
 from PyQt6.QtWidgets import QGraphicsItem
+from PyQt6.QtGui import QPixmap, QPainter, QPen, QColor
 
 
 class Line(QGraphicsItem):
+    texture: QPixmap
     def __init__(self, *args, vertex1: QGraphicsItem, vertex2: QGraphicsItem, **kwargs) -> None:
         pass
+
+    def fill_texture(self):
+        painter = QPainter(self.texture)
+        rect = self.texture.rect()
+        painter.drawLine(rect.x() + rect.width()/2, rect.y(),
+                         rect.x() + rect.width()/2, rect.y() + rect.height())
