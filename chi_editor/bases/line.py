fa5bcf735cc1d00920ff7bc139fd0ad6dc6e8b47
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-09 23:26:01 +0700

    Added rotation and height calculation. Added width and height fields

--- chi_editor/bases/line.py
+++ chi_editor/bases/line.py
@@ -1,4 +1,4 @@
-from math import fabs
+from math import fabs, atan2, sin
 from pathlib import Path
 
 from PyQt6.QtWidgets import QGraphicsItem, QGraphicsPixmapItem
@@ -11,6 +11,8 @@ class Line(QGraphicsPixmapItem):
     vertex2: QGraphicsItem
     end1: QPointF
     end2: QPointF
+    width: float
+    height: float
     MAX_WIDTH = 30
 
     def __init__(self, *args, vertex1: QGraphicsItem, vertex2: QGraphicsItem, **kwargs) -> None:
@@ -24,14 +26,22 @@ class Line(QGraphicsPixmapItem):
         self.end1 = QPointF(vertex2.x() + vertex2.boundingRect().width() / 2,
                             vertex2.y() + vertex2.boundingRect().height() / 2)
 
+        # line will be rotated around its vertices
+        self.setTransformOriginPoint(self.end1)
+
+        self.setRotation(atan2(self.end1.y() - self.end2.y(), self.end1.x() - self.end2.x()))
+
         # min of MAX_WIDTH and boarders of vertex items
-        pixmap_width = min([self.MAX_WIDTH, vertex1.boundingRect().width(), vertex1.boundingRect().height(),
+        self.width = min([self.MAX_WIDTH, vertex1.boundingRect().width(), vertex1.boundingRect().height(),
                             vertex2.boundingRect().width(), vertex2.boundingRect().height()])
 
-        # height is just distance between y's
-        self.setPixmap(QPixmap(pixmap_width, fabs(self.end1.y() - self.end1.y())))
+        # hypotenuse of right triangle of vertices, rotation is an angle between them
+        self.height = fabs(self.end1.y() - self.end1.y()) / sin(self.rotation())
 
-    def fill_texture(self):
+        # height is just distance between y's
+        self.setPixmap(QPixmap(self.width, self.height))
+
+    def paint(self):
         painter = QPainter(self.pixmap())
         rect = self.texture.rect()
         painter.drawLine(rect.x() + rect.width() / 2, rect.y(),
