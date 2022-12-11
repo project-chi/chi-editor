7c83152577dcdf9df4d9031b141d37f0e7f56b03
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-11 20:31:10 +0700

    Added tools __init__ to include atom tool

--- chi_editor/toolbar/tools/__init__.py
+++ chi_editor/toolbar/tools/__init__.py
@@ -1,11 +1,13 @@
 from .arrow import Arrow
 from .text import Text
 from .bond import Bond
+from .atom import Atom
 from ...bases.tool import Tool
 
 
 tools: tuple[type[Tool], ...] = (
     Arrow,
     Text,
-    Bond
+    Bond,
+    Atom
 )
