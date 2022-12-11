b0b48da71efe15ec521289edf8556180cd354dbc
tobaffoon <a.mazyar@g.nsu.ru>
2022-12-11 16:02:16 +0700

    All position methods now operate on scene coordinates. Fixed angle calculation

--- chi_editor/bases/line.py
+++ chi_editor/bases/line.py
@@ -1,5 +1,4 @@
-from math import fabs, atan2, sin, degrees, radians
-from pathlib import Path
+from math import fabs, atan2, cos, degrees, radians
 
 from PyQt6.QtWidgets import QGraphicsItem, QGraphicsPixmapItem, QWidget, QStyleOptionGraphicsItem
 from PyQt6.QtGui import QPixmap, QPainter
@@ -13,12 +12,14 @@ class Line(QGraphicsPixmapItem):
     height: float
     MAX_WIDTH = 30
 
-    def __init__(self, *args, vertex1: QGraphicsItem, vertex2: QGraphicsItem, **kwargs) -> None:
+    def __init__(self, vertex1: QGraphicsItem, vertex2: QGraphicsItem, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.vertex1 = vertex1
         self.vertex2 = vertex2
 
-        self.setPos(vertex1.boundingRect().center())
+        self.setShapeMode(QGraphicsPixmapItem.ShapeMode.BoundingRectShape)
+
+        self.setPos(vertex1.sceneBoundingRect().center() - QPointF(vertex1.sceneBoundingRect().width() / 2, 0))
 
         # min of MAX_WIDTH and boarders of vertex items
         self.width = min([self.MAX_WIDTH, vertex1.boundingRect().width(), vertex1.boundingRect().height(),
@@ -28,23 +29,24 @@ class Line(QGraphicsPixmapItem):
 
     # recalculate height and rotation of pixmap
     def __update_pixmap(self, moved_vertex: QGraphicsItem) -> None:
-        # if (moved_vertex is not self.vertex1) or (moved_vertex is not self.vertex2):
-        #     raise ValueError(f'Can\'t call __update_pixmap with {moved_vertex} argument')
-
         # simple deduction of static vertex
-        moved_point = moved_vertex.boundingRect().center()
+        moved_point = moved_vertex.sceneBoundingRect().center()
+
         if moved_vertex is self.vertex1:
-            static_point = self.vertex2.boundingRect().center()
+            static_point = self.vertex2.sceneBoundingRect().center()
         else:
-            static_point = self.vertex1.boundingRect().center()
+            static_point = self.vertex1.sceneBoundingRect().center()
 
         # line will be rotated around its vertices
-        self.setTransformOriginPoint(static_point)
+        self.setTransformOriginPoint(self.mapFromScene(static_point))
 
-        self.setRotation(degrees(atan2(static_point.y() - moved_point.y(), static_point.x() - moved_point.x())))
+        self.setRotation(-1 * degrees(atan2(moved_point.x() - static_point.x(), moved_point.y() - static_point.y())))
 
         # hypotenuse of right triangle of vertices, rotation is an angle between them
-        self.height = fabs((static_point.y() - moved_point.y()) / sin(radians(self.rotation())))
+        if self.rotation() == 0:
+            self.height = fabs(static_point.y() - moved_point.y())
+        else:
+            self.height = fabs((static_point.y() - moved_point.y()) / cos(radians(self.rotation())))
 
         # height is just distance between y's
         self.setPixmap(QPixmap(int(self.width), int(self.height)))
@@ -52,14 +54,13 @@ class Line(QGraphicsPixmapItem):
         self.update()
 
     def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem = None, widget: QWidget = None) -> None:
-        rect = self.pixmap().rect()
         pen = painter.pen()
-        pen.setWidth(5)
+        pen.setWidth(3)
         painter.setPen(pen)
 
         # draw straight line in the parallel to y-axis
-        painter.drawLine(QPointF(rect.x() + rect.width() / 2, rect.y()),
-                         QPointF(rect.x() + rect.width() / 2, rect.y() + rect.height()))
+        painter.drawLine(QPointF(self.width / 2, 0),
+                         QPointF(self.width / 2, self.height))
 
     def boundingRect(self) -> QRectF:
-        return QRectF(self.pixmap().rect())
+        return super(Line, self).boundingRect()
