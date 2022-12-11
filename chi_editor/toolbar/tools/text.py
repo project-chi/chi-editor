04633b5c096af44c638014cf2b87b81a3f248a37
andrey-dru-me1 <a.melnikov4@g.nsu.ru>
2022-12-11 19:55:50 +0700

    Resolve some issues

--- chi_editor/toolbar/tools/text.py
+++ chi_editor/toolbar/tools/text.py
@@ -1,12 +1,12 @@
 from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QFont, QKeyEvent, QInputEvent, QFocusEvent
+from PyQt6.QtGui import QFont, QKeyEvent, QFocusEvent
 from PyQt6.QtWidgets import QGraphicsSceneMouseEvent, QGraphicsTextItem, QGraphicsItem
 
 from ...bases.tool import Tool
 
 class MyText(QGraphicsTextItem):
 
-    def mouseDoubleClickEvent(self, event: 'QGraphicsSceneMouseEvent') -> None:
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent) -> None:
         self.setTextInteractionFlags(Qt.TextInteractionFlag.TextEditable)
         self.setFocus(Qt.FocusReason.MouseFocusReason)
 
@@ -24,7 +24,7 @@ class Text(Tool):
     def mouse_press_event(self, event: QGraphicsSceneMouseEvent) -> None:
         new_text = MyText()
         new_text.setPos(event.scenePos())
-        new_text.setPlainText("H")
+        new_text.setPlainText('H')
         new_text.setFont(QFont('Impact'))
         new_text.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
 
