579c89c7e48b82b8298cbf9e9e3e148561508f82
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-12-13 21:55:56 +0700

    fix eraser
    left click -- delete selected item
    right click -- clear canvas

--- chi_editor/toolbar/tools/eraser.py
+++ chi_editor/toolbar/tools/eraser.py
@@ -6,10 +6,13 @@ from ...bases.tool import Tool
 
 class Eraser(Tool):
     def mouse_press_event(self, event: QGraphicsSceneMouseEvent) -> None:
-        items = self.canvas.items(event.scenePos(), Qt.ItemSelectionMode.IntersectsItemShape)
-        if not items:
-            return super(Eraser, self).mouse_press_event(event)
-        self.canvas.removeItem(items[0])
+        if event.button() == Qt.MouseButton.LeftButton:
+            items = self.canvas.items(event.scenePos(), Qt.ItemSelectionMode.IntersectsItemShape)
+            if not items:
+                return super(Eraser, self).mouse_press_event(event)
+            self.canvas.removeItem(items[0])
+        else:
+            self.canvas.clear()
 
 
     @property
