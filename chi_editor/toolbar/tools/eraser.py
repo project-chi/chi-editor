b2c748aa98121272853a0356eef5d98e685d485b
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2022-12-13 21:52:08 +0700

    fix eraser

--- chi_editor/toolbar/tools/eraser.py
+++ chi_editor/toolbar/tools/eraser.py
@@ -7,7 +7,7 @@ from ...bases.tool import Tool
 class Eraser(Tool):
     def mouse_press_event(self, event: QGraphicsSceneMouseEvent) -> None:
         items = self.canvas.items(event.scenePos(), Qt.ItemSelectionMode.IntersectsItemShape)
-        if items == []:
+        if not items:
             return super(Eraser, self).mouse_press_event(event)
         self.canvas.removeItem(items[0])
 
