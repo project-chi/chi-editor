6526a345dd85db9abdea0534a1744d1a194ab3db
tobaffoon <a.mazyar@g.nsu.ru>
2023-02-02 10:33:26 +0700

    Add wedge bond tool

--- chi_editor/toolbar/tools/__init__.py
+++ chi_editor/toolbar/tools/__init__.py
@@ -2,6 +2,7 @@ from .arrow import Arrow
 from .bonds.create_double_bond import CreateDoubleBond
 from .bonds.create_single_bond import CreateSingleBond
 from .bonds.create_triple_bond import CreateTripleBond
+from .bonds.create_wedge_bond import CreateWedgeBond
 from .eraser import Eraser
 from .nitrogen import Nitrogen
 from .oxygen import Oxygen
@@ -18,6 +19,7 @@ tools: tuple[type[Tool], ...] = (
     CreateSingleBond,
     CreateDoubleBond,
     CreateTripleBond,
+    CreateWedgeBond,
     Atom,
     Nitrogen,
     Oxygen,
---
+++ chi_editor/toolbar/tools/bonds/create_wedge_bond.py
@@ -0,0 +1,16 @@
+from PyQt6.QtWidgets import QGraphicsItem
+from PyQt6.QtCore import QPointF
+
+from ..bond import Bond
+from ....bases.line import Line
+from ....chem_bonds.wedge_bond import WedgeBond
+
+
+class CreateWedgeBond(Bond):
+
+    def get_line(self, start_atom: QGraphicsItem, mouse_pos: QPointF) -> Line:
+        return WedgeBond(start_atom, mouse_pos)
+
+    @property
+    def asset(self) -> str:
+        return 'bond1'
