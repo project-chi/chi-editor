a8c9377301a5d1c99cc3f40560e11156ac2890ca
tobaffoon <a.mazyar@g.nsu.ru>
2023-02-04 22:36:30 +0700

    Remove redundant comments

--- chi_editor/bases/line.py
+++ chi_editor/bases/line.py
@@ -37,32 +37,26 @@ class Line(QGraphicsPixmapItem):
     def set_v2(self, end: QGraphicsItem) -> None:
         self.vertex2 = end
 
-    # recalculate height and rotation of pixmap
     def update_pixmap(self, moved_vertex: QGraphicsItem, following_mouse: bool = False) -> None:
-        # simple deduction of static vertex
         moved_point = moved_vertex.sceneBoundingRect().center()
         self.setRotation(0)
         self.setScale(1)
 
-        # moved_vertex is second vertex or bond follows the mouse
         if moved_vertex is self.vertex2 or following_mouse:
             static_point = self.vertex1.sceneBoundingRect().center()
         else:
             static_point = self.vertex2.sceneBoundingRect().center()
-        self.setPos(static_point - QPointF(self.width / 2, 0))
-
-        # line will be rotated around its vertices
         self.setTransformOriginPoint(self.mapFromScene(static_point))
+
+        self.setPos(static_point - QPointF(self.width / 2, 0))
 
         self.setRotation(-1 * degrees(atan2(moved_point.x() - static_point.x(), moved_point.y() - static_point.y())))
 
-        # hypotenuse of right triangle of vertices, rotation is an angle between them
         if self.rotation() % 360 == 90 or self.rotation() % 360 == 270:
             self.height = fabs(static_point.x() - moved_point.x())
         else:
             self.height = fabs((static_point.y() - moved_point.y()) / cos(radians(self.rotation())))
 
-        # height is just distance between y's
         self.setPixmap(QPixmap(int(self.width), int(self.height)))
 
         self.update()
@@ -73,14 +67,11 @@ class Line(QGraphicsPixmapItem):
         painter.restore()
 
     def paint_line(self, painter: QPainter) -> None:
-        """
-        This method determines how line is drawn.
-        It should be overrided by children classes
-        """
+        """This method determines how line is drawn.
+        It should be overrided by children classes"""
         pen = QPen(QColor("black"), 3)
         painter.setPen(pen)
 
-        # draw straight line
         painter.drawLine(QPointF(self.width / 2, 0),
                          QPointF(self.width / 2, self.height))
 
