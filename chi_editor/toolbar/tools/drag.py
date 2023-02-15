65f86bb6f84b8ce49baf1843447a57b04401b593
tobaffoon <a.mazyar@g.nsu.ru>
2023-02-15 10:11:33 +0700

    Move drag mode change to respective tool code

--- chi_editor/toolbar/tools/drag.py
+++ chi_editor/toolbar/tools/drag.py
@@ -1,17 +1,19 @@
-from PyQt6.QtWidgets import QGraphicsSceneMouseEvent
+from PyQt6.QtWidgets import QGraphicsSceneMouseEvent, QGraphicsView
 
 from ...bases.tool import Tool
 
 
 class Drag(Tool):
     def mouse_press_event(self, event: QGraphicsSceneMouseEvent) -> None:
-        pass
+        for view in self.canvas.views():
+            view.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
 
     def mouse_move_event(self, event: QGraphicsSceneMouseEvent) -> None:
         pass
 
     def mouse_release_event(self, event: QGraphicsSceneMouseEvent) -> None:
-        pass
+        for view in self.canvas.views():
+            view.setDragMode(QGraphicsView.DragMode.NoDrag)
 
     @property
     def asset(self) -> str:
