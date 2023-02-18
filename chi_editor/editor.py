9fcb143972bf2c7e030f7db3ff5a791edeb2b442
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-02-18 13:55:18 +0700

    Add scale
    currently not working

--- chi_editor/editor.py
+++ chi_editor/editor.py
@@ -1,6 +1,7 @@
 from PyQt6.QtCore import Qt, QRectF
-from PyQt6.QtGui import QIcon
-from PyQt6.QtWidgets import QMainWindow, QGraphicsView, QSizePolicy
+from PyQt6.QtGui import QIcon, QColor, QTransform
+from PyQt6.QtWidgets import QMainWindow, QGraphicsView, QSizePolicy, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, \
+    QStackedLayout
 
 from .canvas import Canvas
 from .constants import ASSETS
@@ -8,21 +9,75 @@ from .toolbar import CanvasToolBar
 
 
 class Editor(QMainWindow):
+    canvas: Canvas
+    graphics_view: QGraphicsView
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        canvas = Canvas(self)
-        canvas.setSceneRect(QRectF(self.geometry()))
+        self.canvas = Canvas(self)
+        self.canvas.setSceneRect(QRectF(self.geometry()))
 
-        graphics_view = QGraphicsView(self)
-        graphics_view.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-        graphics_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        graphics_view.setScene(canvas)
-        graphics_view.setViewportUpdateMode(QGraphicsView.ViewportUpdateMode.FullViewportUpdate)
+        self.graphics_view = QGraphicsView(self)
+        self.graphics_view.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
+        self.graphics_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+        self.graphics_view.setScene(self.canvas)
+        self.graphics_view.setViewportUpdateMode(QGraphicsView.ViewportUpdateMode.FullViewportUpdate)
 
         self.setWindowTitle("Project Chi")
         self.setWindowIcon(QIcon(str(ASSETS / 'project-chi.png')))
         self.resize(1000, 800)
 
-        self.addToolBar(Qt.ToolBarArea.LeftToolBarArea, CanvasToolBar(canvas=canvas))
-        self.setCentralWidget(graphics_view)
+        self.addToolBar(Qt.ToolBarArea.LeftToolBarArea, CanvasToolBar(canvas=self.canvas))
+        # self.setCentralWidget(self.graphics_view)
+
+        # Create a button that changes the scale of the view
+        scale_plus = QPushButton("Zoom In", self)
+        scale_plus.clicked.connect(self.zoom_in)
+
+        scale_minus = QPushButton("Zoom Out", self)
+        scale_minus.clicked.connect(self.zoom_out)
+
+        # Create a vertical box layout to hold the buttons
+        vbox = QVBoxLayout()
+        vbox.addStretch()
+        vbox.addWidget(scale_minus)
+        vbox.addWidget(scale_plus)
+
+        # Create a horizontal box layout to hold the vertical layout
+        hbox = QHBoxLayout()
+        hbox.addStretch()
+        hbox.addLayout(vbox)
+
+        # Create a widget to hold the horizontal layout
+        widget = QWidget()
+        widget.setLayout(hbox)
+
+        # Create a stacked layout to hold the view and the widget with buttons
+        stack = QStackedLayout()
+        stack.addWidget(self.graphics_view)
+        stack.addWidget(widget)
+
+        # Set the stacked layout as the central widget of the main window
+        central_widget = QWidget()
+        central_widget.setLayout(stack)
+        self.setCentralWidget(central_widget)
+
+        # Set the view to be interactable
+        self.graphics_view.setInteractive(True)
+
+    def zoom_in(self):
+        # Get the current scale factor of the view
+        current_scale = self.graphics_view.transform().m11()
+
+        # Update the scale factor of the view
+        new_scale = current_scale * 1.2
+        self.graphics_view.setTransform(QTransform.fromScale(new_scale, new_scale))
+
+    def zoom_out(self):
+        # Get the current scale factor of the view
+        current_scale = self.graphics_view.transform().m11()
+
+        # Update the scale factor of the view
+        new_scale = current_scale / 1.2
+        self.graphics_view.setTransform(QTransform.fromScale(new_scale, new_scale))
