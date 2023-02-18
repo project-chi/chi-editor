702c079211056d2480bd1ecb0b0a035f8277ecbd
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-02-18 20:09:31 +0700

    Clean code a little bit

--- chi_editor/editor.py
+++ chi_editor/editor.py
@@ -29,39 +29,24 @@ class Editor(QMainWindow):
         self.resize(1000, 800)
 
         self.addToolBar(Qt.ToolBarArea.LeftToolBarArea, CanvasToolBar(canvas=self.canvas))
-        # self.setCentralWidget(self.graphics_view)
 
-        # Create a button that changes the scale of the view
         scale_plus = QPushButton("Zoom In", self)
         scale_plus.clicked.connect(self.zoom_in)
-
         scale_minus = QPushButton("Zoom Out", self)
         scale_minus.clicked.connect(self.zoom_out)
-
-        # Create a vertical box layout to hold the buttons
         vbox = QVBoxLayout()
         vbox.addStretch()
         vbox.addWidget(scale_minus)
         vbox.addWidget(scale_plus)
-
-        # Create a horizontal box layout to hold the vertical layout
         hbox = QHBoxLayout()
         hbox.addStretch()
         hbox.addLayout(vbox)
-
         layout = QVBoxLayout()
         layout.addWidget(self.graphics_view)
         layout.addLayout(hbox)
-
-        # Create a central widget and set its layout
         central_widget = QWidget()
         central_widget.setLayout(layout)
-
-        # Set the central widget of the QMainWindow
         self.setCentralWidget(central_widget)
-
-        # Set the view to be interactable
-        self.graphics_view.setInteractive(True)
 
     def zoom_in(self):
         # Get the current scale factor of the view
