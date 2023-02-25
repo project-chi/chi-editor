9c574172263f25cbe383d4b2ba3c1336414d1b4d
andrey <a.melnikov4@g.nsu.ru>
2023-02-25 18:43:34 +0700

    move zoom in/zoom out buttons onto the canvas (without structured code yet)

--- chi_editor/editor.py
+++ chi_editor/editor.py
@@ -1,7 +1,7 @@
 from PyQt6.QtCore import Qt, QRectF
-from PyQt6.QtGui import QIcon, QColor, QTransform
+from PyQt6.QtGui import QIcon, QColor, QTransform, QBrush
 from PyQt6.QtWidgets import QMainWindow, QGraphicsView, QSizePolicy, QPushButton, QVBoxLayout, QHBoxLayout, QWidget, \
-    QStackedLayout
+    QStackedLayout, QBoxLayout
 
 from .canvas import Canvas
 from .constants import ASSETS
@@ -9,44 +9,78 @@ from .toolbar import CanvasToolBar
 
 
 class Editor(QMainWindow):
-    canvas: Canvas
+
+    # Hierarchy:
+    #
+    #   window:
+    #       workspace:
+    #           graphics_view:
+    #               canvas:
+    #       toolbar:
+
+    # Contains everything except toolbar
+    workspace: QWidget
+
+    # QGraphicsView contains drawable space
     graphics_view: QGraphicsView
+
+    # GraphicsScene where to draw all graphical objects
+    canvas: Canvas
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.canvas = Canvas(self)
-        self.canvas.setSceneRect(QRectF(self.geometry()))
-
-        self.graphics_view = QGraphicsView(self)
-        self.graphics_view.setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)
-        self.graphics_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
-        self.graphics_view.setScene(self.canvas)
-        self.graphics_view.setViewportUpdateMode(QGraphicsView.ViewportUpdateMode.FullViewportUpdate)
-
+        # Window settings
         self.setWindowTitle("Project Chi")
         self.setWindowIcon(QIcon(str(ASSETS / 'project-chi.png')))
         self.resize(1000, 800)
 
-        self.addToolBar(Qt.ToolBarArea.LeftToolBarArea, CanvasToolBar(canvas=self.canvas))
+        # The biggest part of interface
+        self.workspace = QWidget()  # create workspace
+
+        # Initialize QGraphicsView
+        self.graphics_view = QGraphicsView(self)    # create QGraphicsView
+        self.graphics_view\
+            .setAlignment(Qt.AlignmentFlag.AlignLeft | Qt.AlignmentFlag.AlignTop)     # Set QGraphicsView position
+
+        # Initialize GGraphicsScene called canvas
+        self.canvas = Canvas(self)
+        self.canvas.setSceneRect(QRectF(self.geometry()))
+
+        # Bind GraphicsScene to GraphicsView
+        self.graphics_view.setScene(self.canvas)
+
+        h_button_group = QHBoxLayout(self)
+
+        # Box contains magnifying glass
+        v_button_group = QVBoxLayout(self)
+        v_button_group.addStretch(1)
+        v_button_group.setAlignment(Qt.AlignmentFlag.AlignRight)
 
         scale_plus = QPushButton("Zoom In", self)
         scale_plus.clicked.connect(self.zoom_in)
+
         scale_minus = QPushButton("Zoom Out", self)
         scale_minus.clicked.connect(self.zoom_out)
-        vbox = QVBoxLayout()
-        vbox.addStretch()
-        vbox.addWidget(scale_minus)
-        vbox.addWidget(scale_plus)
-        hbox = QHBoxLayout()
-        hbox.addStretch()
-        hbox.addLayout(vbox)
-        layout = QVBoxLayout()
+
+        v_button_group.addWidget(scale_minus)
+        v_button_group.addWidget(scale_plus)
+        v_button_group.addStretch(1)
+
+        h_button_group.addStretch()
+        h_button_group.addLayout(v_button_group)
+        h_button_group.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        layout = QVBoxLayout(self)
         layout.addWidget(self.graphics_view)
-        layout.addLayout(hbox)
-        central_widget = QWidget()
-        central_widget.setLayout(layout)
-        self.setCentralWidget(central_widget)
+
+        self.graphics_view.setLayout(h_button_group)
+
+        self.workspace.setLayout(layout)
+        self.setCentralWidget(self.workspace)
+
+        # Add left toolbar
+        self.addToolBar(Qt.ToolBarArea.LeftToolBarArea, CanvasToolBar(canvas=self.canvas))
 
     def zoom_in(self):
         # Get the current scale factor of the view
