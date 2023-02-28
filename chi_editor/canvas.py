6dd81509a3940868f987ec98093d5ed79f52b6dd
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-01 00:12:52 +0700

    Remade sceneRect methods

--- chi_editor/canvas.py
+++ chi_editor/canvas.py
@@ -1,4 +1,5 @@
 from typing import TypeVar
+from typing import overload
 
 from PyQt6.QtCore import QRectF
 from PyQt6.QtWidgets import QGraphicsScene, QGraphicsSceneMouseEvent
@@ -9,9 +10,11 @@ Tool = TypeVar('Tool')
 
 class Canvas(QGraphicsScene):
     current_action: Tool
+    min_scene_rect: QRectF
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self.min_scene_rect = super().sceneRect()
 
     def mousePressEvent(self, event: QGraphicsSceneMouseEvent) -> None:
         self.current_action.mouse_press_event(event)
@@ -22,5 +25,25 @@ class Canvas(QGraphicsScene):
     def mouseReleaseEvent(self, event: QGraphicsSceneMouseEvent) -> None:
         self.current_action.mouse_release_event(event)
 
-    def minSceneRect(self) -> QRectF:
-        return super().sceneRect()
+    def sceneRect(self) -> QRectF:
+        return self.min_scene_rect
+
+    @overload
+    def setSceneRect(self, sceneRect: QRectF) -> None:
+        ...
+
+    def enlargeScene(self, sceneRect: QRectF) -> None:
+        self.min_scene_rect = self.min_scene_rect.united(sceneRect)
+
+    @overload
+    def setSceneRect(self, x: float, y: float, w: float, h: float) -> None:
+        ...
+
+    def setSceneRect(self, *args) -> None:
+        match args:
+            case [QRectF() as sceneRect]:
+                self.enlargeScene(sceneRect)
+            case [float() as x, float() as y, float() as w, float() as h]:
+                self.enlargeScene(QRectF(x, y, w, h))
+            case _:
+                raise TypeError("wrong signature")
--- chi_editor/canvas_view.py
+++ chi_editor/canvas_view.py
@@ -5,4 +5,4 @@ from PyQt6.QtGui import QResizeEvent
 class CanvasView(QGraphicsView):
     def resizeEvent(self, event: QResizeEvent) -> None:
         super().resizeEvent(event)
-        self.scene().setSceneRect(0, 0, event.size().width(), event.size().height())
+        self.scene().setSceneRect(float(0), float(0), float(event.size().width()), float(event.size().height()))
