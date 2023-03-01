e585c1f3470a68322b595721dfd61ab6286a80e2
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-01 10:27:52 +0700

    Make initial changes to support infinite drag

--- chi_editor/canvas_view.py
+++ chi_editor/canvas_view.py
@@ -1,8 +1,21 @@
+from PyQt6.QtCore import QPoint
 from PyQt6.QtWidgets import QGraphicsView
-from PyQt6.QtGui import QResizeEvent
+from PyQt6.QtGui import QResizeEvent, QMouseEvent
 
 
 class CanvasView(QGraphicsView):
+    old_drag_pos: QPoint
+
     def resizeEvent(self, event: QResizeEvent) -> None:
         super().resizeEvent(event)
         self.scene().setSceneRect(float(0), float(0), float(event.size().width()), float(event.size().height()))
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:
+        super().mousePressEvent(event)
+        self.old_drag_pos = event.scenePosition()
+
+    def mouseMoveEvent(self, event: QMouseEvent) -> None:
+        if self.dragMode() != QGraphicsView.DragMode.ScrollHandDrag:
+            super().mouseMoveEvent(event)
+        else:
+            super().mouseMoveEvent(event)
