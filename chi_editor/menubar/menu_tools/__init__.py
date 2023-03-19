e74820d8fd040064ae6a6b7a94c38653a8b1a869
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-19 20:34:37 +0700

    Add free mode tool to menu tools list

--- chi_editor/menubar/menu_tools/__init__.py
+++ chi_editor/menubar/menu_tools/__init__.py
@@ -1,8 +1,10 @@
 from .create_task import CreateTask
 from .solve_task import SolveTask
+from .free_mode import FreeMode
 from ...bases.menu_tool import MenuTool
 
 menu_tools: tuple[type[MenuTool], ...] = (
     CreateTask,
-    SolveTask
+    SolveTask,
+    FreeMode,
 )
