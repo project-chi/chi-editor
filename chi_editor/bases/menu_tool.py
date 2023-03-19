d695f3988863ff2c0e7795ac8d3ac15166bc2909
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-19 15:25:26 +0700

    Implement menu tool class

--- chi_editor/bases/menu_tool.py
+++ chi_editor/bases/menu_tool.py
@@ -1,22 +1,33 @@
 from PyQt6.QtGui import QAction, QIcon
 
-from ..canvas import Canvas
 from ..constants import RESOURCES
+from ..editor import Editor
 
 
 class MenuTool(QAction):
-    def __init__(self, *args, canvas: Canvas, **kwargs) -> None:
+    _editor: Editor
+    
+    def __init__(self, *args, editor: Editor, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.canvas = canvas
+        self._editor = editor
 
         icon_path = RESOURCES / 'assets' / 'toolbar' / f'{self.asset}.png'
         if icon_path.exists():
             self.setIcon(QIcon(str(icon_path)))
 
-    def activate(self, event: QAction.ActionEvent) -> None:
+        self.setText(self.text_asset)
+
+        self.triggered().connect(self.exec())
+
+    def exec(self) -> None:
         pass
 
     @property
     def asset(self) -> str:
-        """Returns the name of the icon for this tool without extension."""
+        """Returns the name of the icon for this menu tool without extension."""
+        raise NotImplemented
+
+    @property
+    def text_asset(self) -> str:
+        """Returns the text of this tool."""
         raise NotImplemented
