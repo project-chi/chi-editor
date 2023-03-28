ce254c12bc13126c378e40d6b50f1e4c9e17079d
tobaffoon <a.mazyar@g.nsu.ru>
2023-03-29 02:05:50 +0700

    Create class to show result of task check

---
+++ chi_editor/task_result_dialog.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+
+from PyQt6.QtWidgets import QDialog, QLabel
+
+if TYPE_CHECKING:
+    from .editor import Editor
+
+
+class ChooseTaskDialog(QDialog):
+    # Main window
+    editor: "Editor"
+
+    # Text label
+    _label: QLabel
+
+    def __init__(self, *args, editor: "Editor", **kwargs):
+        super().__init__(*args, **kwargs)
+        self.editor = editor
+        self._label = QLabel(self)
+
+    def setText(self, text: str) -> None:
+        self._label.setText(text)
