ec1cc43d1db56b9ee6148a0afa303333ab7e4cc2
andrey <a.melnikov4@g.nsu.ru>
2023-03-29 09:39:48 +0700

    Cancel molecule anchore removal

--- chi_editor/toolbar/tools/eraser.py
+++ chi_editor/toolbar/tools/eraser.py
@@ -1,22 +1,24 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QGraphicsSceneMouseEvent
 
+from ...bases.alpha_atom import AlphaAtom
+from ...bases.line import Line
 from ...bases.tool import Tool
 
 
 class Eraser(Tool):
-
     def mouse_press_event(self, event: QGraphicsSceneMouseEvent) -> None:
         if event.button() == Qt.MouseButton.LeftButton:
-            items = self.canvas.items(event.scenePos(), Qt.ItemSelectionMode.IntersectsItemShape)
+            items = self.canvas.items(
+                event.scenePos(), Qt.ItemSelectionMode.IntersectsItemShape
+            )
             if not items:
                 return super(Eraser, self).mouse_press_event(event)
-            else:
+            elif isinstance(items[0], AlphaAtom) or isinstance(items[0], Line):
                 items[0].remove()
         else:
             self.canvas.clear()
 
-
     @property
     def asset(self) -> str:
-        return 'eraser'
+        return "eraser"
