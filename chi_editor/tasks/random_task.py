8de97ba218f512c90a314da0ff8b84445c41ee75
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-04-02 23:33:22 +0700

    Add return value type to random task constructor

--- chi_editor/tasks/random_task.py
+++ chi_editor/tasks/random_task.py
@@ -5,9 +5,8 @@ from chi_editor.tasks import Task, TaskType
 
 
 class RandomTask(Task):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         server = Server("http://kapkekes.site:8000")
         task_ids = server.get_tasks_identifiers()
         task = server.get_task(choice(task_ids))
         super().__init__(task.name, task.problem, task.solution, task.kind)
-
