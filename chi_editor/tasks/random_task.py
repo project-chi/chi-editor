4a04dabee736a06d63733b6819fd56904f730a56
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-04-02 23:15:13 +0700

    Fix toml

--- chi_editor/tasks/random_task.py
+++ chi_editor/tasks/random_task.py
@@ -1,8 +1,13 @@
+from random import choice
+
+from chi_editor.api.server import Server
 from chi_editor.tasks import Task, TaskType
 
 
 class RandomTask(Task):
     def __init__(self, *args, **kwargs):
         server = Server("http://kapkekes.site:8000")
-        super().__init__("Benzene", "Draw a benzene", "c1ccccc1", TaskType.Random)
+        task_ids = server.get_tasks_identifiers()
+        task = server.get_task(choice(task_ids))
+        super().__init__(task.name, task.problem, task.solution, task.kind)
 
