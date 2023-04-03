84ccc0a79ec7515c83a615d4393947437881ee15
andrey <a.melnikov4@g.nsu.ru>
2023-04-03 20:11:30 +0700

    Make anchor non-removable

--- chi_editor/toolbar/tools/eraser.py
+++ chi_editor/toolbar/tools/eraser.py
@@ -1,21 +1,22 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QGraphicsSceneMouseEvent
 
-from ...bases.tool import Tool
+from chi_editor.bases.molecule import MoleculeAnchor
+from chi_editor.bases.tool import Tool
 
 
 class Eraser(Tool):
-
     def mouse_press_event(self, event: QGraphicsSceneMouseEvent) -> None:
         if event.button() == Qt.MouseButton.LeftButton:
-            items = self.canvas.items(event.scenePos(), Qt.ItemSelectionMode.IntersectsItemShape)
+            items = self.canvas.items(
+                event.scenePos(), Qt.ItemSelectionMode.IntersectsItemShape
+            )
             if not items:
                 return super(Eraser, self).mouse_press_event(event)
-            else:
+            elif not isinstance(items[0], MoleculeAnchor):
                 items[0].remove()
         else:
             self.canvas.clear()
-
 
     @property
     def picture(self) -> str:
