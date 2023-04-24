173b0bbd0299e3c5b396723a6b42a61444f277c0
tobaffoon <a.mazyar@g.nsu.ru>
2023-04-25 00:03:16 +0700

    Add error message on taken task name

--- chi_editor/utils/json_utils.py
+++ chi_editor/utils/json_utils.py
@@ -3,6 +3,7 @@ from typing import ClassVar
 from pathlib import Path
 
 from chi_editor.constants import RESOURCES
+from chi_editor.dialog_windows.error_messages.task_exists import TaskExistsMessageBox
 
 # Default folder for local task files
 default_dir: ClassVar[Path] = RESOURCES / "local_tasks"
@@ -20,5 +21,8 @@ def create_task(name: str, kind: int, problem: str, solution: str, initial: str)
 
     json_object = json.dumps(dictionary, indent=2)
 
-    with open(str(default_dir.absolute() / (name + ".json")), "w") as outfile:
-        outfile.write(json_object)
+    try:
+        with open(str(default_dir.absolute() / (name + ".json")), "x") as outfile:
+            outfile.write(json_object)
+    except FileExistsError:
+        TaskExistsMessageBox().exec()
