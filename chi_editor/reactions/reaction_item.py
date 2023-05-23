3a80560fed2b73e2d46ad31e02f7046fe2d3ed09
tobaffoon <a.mazyar@g.nsu.ru>
2023-05-24 02:18:48 +0700

    Add arrow

--- chi_editor/reactions/reaction_item.py
+++ chi_editor/reactions/reaction_item.py
@@ -1,15 +1,18 @@
+from typing import Optional, ClassVar
+
 from PyQt6.QtCore import QSizeF, QRectF, QPointF
+from PyQt6.QtGui import QPainter, QPainterPath
 from PyQt6.QtWidgets import (
     QGraphicsItem,
     QGraphicsItemGroup,
     QGraphicsEllipseItem,
-    QGraphicsSceneHoverEvent
+    QGraphicsSceneHoverEvent,
+    QStyleOptionGraphicsItem,
+    QWidget,
 )
 
 from chi_editor.reactions.reaction_model import ReactionModel
-from chi_editor.reactions.reagent_adder import ReagentAdder
-
-_default_add_item_size = QSizeF(40, 40)
+from chi_editor.reactions.reagent_adder import ReagentAdder, GrowthDirection
 
 
 class ReactionItem(QGraphicsItemGroup):
@@ -18,14 +21,24 @@ class ReactionItem(QGraphicsItemGroup):
     _product_items: list[QGraphicsItem] = {}
     _add_reagent_item: QGraphicsEllipseItem
     _add_product_item: QGraphicsEllipseItem
+    add_item_size: ClassVar[QSizeF] = QSizeF(20, 20)
+    reagent_size: ClassVar[QSizeF] = QSizeF(120, 120)
+    plus_size: ClassVar[QSizeF] = QSizeF(20, 20)
+    default_gap: ClassVar[float] = 20
+    arrow_height: ClassVar[float] = 30
+    arrow_width: ClassVar[float] = 60
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
-        self._add_reagent_item = ReagentAdder(self._reagent_items, parent=self)
-        self._add_reagent_item.setRect(QRectF(QPointF(-100, 0), _default_add_item_size))
-        self._add_product_item = ReagentAdder(self._product_items, parent=self)
-        self._add_product_item.setRect(QRectF(QPointF(100, 0), _default_add_item_size))
+        self._add_reagent_item = ReagentAdder(self._reagent_items, GrowthDirection.LEFT, parent=self)
+        self._add_reagent_item.setRect(QRectF(
+            QPointF(-1 * (self.arrow_width / 2 + self.default_gap + self.add_item_size.width()),
+                    -1 * self.add_item_size.height() / 2), self.add_item_size))
+        self._add_product_item = ReagentAdder(self._product_items, GrowthDirection.RIGHT, parent=self)
+        self._add_product_item.setRect(
+            QRectF(QPointF(self.arrow_width / 2 + self.default_gap, -1 * self.add_item_size.height() / 2),
+                   self.add_item_size))
         self.setFiltersChildEvents(False)
         self.setAcceptHoverEvents(False)
 
@@ -43,3 +56,19 @@ class ReactionItem(QGraphicsItemGroup):
             self._add_reagent_item.hoverLeaveEvent(event)
         elif self._add_product_item.sceneBoundingRect().contains(event.lastScenePos()):
             self._add_product_item.hoverLeaveEvent(event)
+
+    def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, widget: Optional[QWidget] = ...) -> None:
+        painter.save()
+
+        self._paint_arrow(painter)
+
+        painter.restore()
+
+    def _paint_arrow(self, painter: QPainter):
+        arrow_path = QPainterPath(QPointF(-1 * self.arrow_width / 2, 0))
+        arrow_path.lineTo(self.arrow_width / 2, 0)
+        arrow_path.lineTo(0, self.arrow_height / 2)
+        arrow_path.lineTo(self.arrow_width / 2, 0)
+        arrow_path.lineTo(0, -1 * self.arrow_height / 2)
+
+        painter.drawPath(arrow_path)
--- chi_editor/reactions/reagent_adder.py
+++ chi_editor/reactions/reagent_adder.py
@@ -1,23 +1,33 @@
 from typing import Optional, ClassVar
+from enum import Enum
 
 from PyQt6.QtCore import QPointF
 from PyQt6.QtWidgets import QGraphicsEllipseItem, QGraphicsSceneMouseEvent, QGraphicsItem, QStyleOptionGraphicsItem, \
-    QWidget, QGraphicsSceneHoverEvent
+    QWidget, QGraphicsSceneHoverEvent, QGraphicsRectItem
 from PyQt6.QtGui import QPainter, QColor
+
+
+class GrowthDirection(Enum):
+    LEFT = 1
+    RIGHT = 2
 
 
 class ReagentAdder(QGraphicsEllipseItem):
     _reagent_list: list[QGraphicsItem]
     _item_highlighted: bool = False
-    _highlight_color: QColor = QColor(0, 255, 50, 100)
+    _highlight_color: ClassVar[QColor] = QColor(0, 255, 50, 100)
+    _growth_direction: GrowthDirection
 
-    def __init__(self, reagent_list: list[QGraphicsItem], *args, **kwargs):
+    def __init__(self, reagent_list: list[QGraphicsItem], growth_direction: GrowthDirection, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._reagent_list = reagent_list
+        self._growth_direction = growth_direction
         self.setAcceptHoverEvents(True)
 
     def mousePressEvent(self, event: QGraphicsSceneMouseEvent):
-        self._reagent_list.append(QGraphicsItem())
+        new_item = QGraphicsRectItem(len(self._reagent_list))
+
+        self._reagent_list.append()
 
     def paint(self, painter: QPainter, option: 'QStyleOptionGraphicsItem', widget: Optional[QWidget] = ...) -> None:
         painter.save()
@@ -40,6 +50,11 @@ class ReagentAdder(QGraphicsEllipseItem):
 
     def _getRightMiddlePoint(self) -> QPointF:
         return self.boundingRect().topRight() + QPointF(0, self.boundingRect().height() / 2)
+
+    # def _getNextPos(self) -> QPointF:
+    #     match self._growth_direction:
+    #         case Direction.LEFT:
+    #             return QPointF(-1 * len(self._reagent_list) * ())
 
     def hoverEnterEvent(self, event: 'QGraphicsSceneHoverEvent') -> None:
         self._item_highlighted = True
