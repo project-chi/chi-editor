88067073096994cb08d32c16a4363569b482095c
MIZINCHIK <66423613+MIZINCHIK@users.noreply.github.com>
2023-05-24 00:44:18 +0700

    Fix error handler

--- chi_editor/main.py
+++ chi_editor/main.py
@@ -1,18 +1,8 @@
 import sys
 
-from PyQt6.QtWidgets import QApplication, QMessageBox
+from PyQt6.QtWidgets import QApplication
 
 from chi_editor.editor import Editor
-
-
-def error_handler(error_message: str) -> None:
-    error_box = QMessageBox()
-    error_box.setIcon(QMessageBox.Icon.Critical)
-    error_box.setWindowTitle("Oopsie Doopsie")
-    error_box.setText(error_message)
-    error_box.setStandardButtons(QMessageBox.StandardButton.Ok)
-    error_box.exec()
-    pass
 
 
 def main():
